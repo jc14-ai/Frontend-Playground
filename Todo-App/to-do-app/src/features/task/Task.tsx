d4525a077fee25d0098f8eb635b4b4ef5fd19396
Jzy <jestalycastillo15@gmail.com>
2025-08-06 01:01:05 +0800

    fix: box mapping with states and props

--- Todo-App/to-do-app/src/features/task/Task.tsx
+++ Todo-App/to-do-app/src/features/task/Task.tsx
@@ -5,33 +5,26 @@ interface Props{
 }
 
 function Task(){
-    // const [task, incTask] = useState(<></>);
-    // const [boxID, countID] = useState<Box[]>([]);
+   
     const [count, countTask] = useState<number>(0);
+    const [box, giveBoxID] = useState<Props[]>([]);
 
-    // const addTask = () => {
-    //     countTask(count => count + 1);
-    //     countID(id => [...id, {id : count}]);
-    // };
+    const addTask = ():void => {
+        countTask(count => count + 1);
+        giveBoxID(box => [...box, {id: count}]);
+    }
 
-    // const removeTask = () => {
-    //     countTask(count => count > 0 ? count - 1 : count)
-    // }
-    // const addTask = () => {
-    //     incTask(<>
-    //     {task}
-    //     <div className='my-task'>
-    //         <button >done</button>
-    //         <button>remove</button>
-    //     </div>
-    //     </>)
-    // };
+    const removeTask = (id:number) => {
+        giveBoxID(boxes => boxes.filter(box => box.id !== id))
+    }
 
     return <div className='Task'>
         <input placeholder='To do' className='task-input'></input>
-        <button className='add-task-button' >Add Task</button>
+        <button className='add-task-button' onClick={addTask}>Add Task</button>
         <div className='task-container'>{
-        [...Array(count)].map((_,i) => <div><button >remove {i}</button></div>)
+        box.map((box) => <div key={box.id}>
+            <button onClick={() => removeTask(box.id)}>remove {box.id}</button>
+            </div>)
         }</div>
         <div className='done-task-container'></div>
     </div>
