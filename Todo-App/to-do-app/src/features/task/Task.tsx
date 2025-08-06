3dd4b1452f018e4f576f6de2e3d4e649347e9688
Jzy <jestalycastillo15@gmail.com>
2025-08-06 15:01:51 +0800

    Added done task mapper

--- Todo-App/to-do-app/src/features/task/Task.tsx
+++ Todo-App/to-do-app/src/features/task/Task.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './Task.css'
 
 interface Props{
     id: number;
@@ -7,26 +8,55 @@ interface Props{
 function Task(){
    
     const [count, countTask] = useState<number>(0);
-    const [box, giveBoxID] = useState<Props[]>([]);
+    const [boxes, giveBoxID] = useState<Props[]>([]);
+    const [doneBoxes, checkBoxID] = useState<Props[]>([]);
 
     const addTask = ():void => {
         countTask(count => count + 1);
         giveBoxID(box => [...box, {id: count}]);
     }
 
+    const checkTask = (id:number) => {
+        checkBoxID(doneBoxes => [...doneBoxes, {id: id}])
+        removeTask(id);
+    }
+
     const removeTask = (id:number) => {
         giveBoxID(boxes => boxes.filter(box => box.id !== id))
     }
 
+    const deleteTask = (id:number) => {
+        checkBoxID(doneBoxes => doneBoxes.filter(doneBox => doneBox.id !== id))
+    }
+
     return <div className='Task'>
-        <input placeholder='To do' className='task-input'></input>
-        <button className='add-task-button' onClick={addTask}>Add Task</button>
+        <input placeholder='To do' className='task-input'/>
+
+        <button className='add-task-button' onClick={addTask}>
+            Add Task
+        </button>
+
         <div className='task-container'>{
-        box.map((box) => <div key={box.id}>
-            <button onClick={() => removeTask(box.id)}>remove {box.id}</button>
-            </div>)
-        }</div>
-        <div className='done-task-container'></div>
+            boxes.map(box => 
+            <div className='my-task' key={box.id}>
+                <button className='remove-task-button' onClick={() => removeTask(box.id)}>
+                    Remove
+                </button>
+
+                <button className='done-task-button' onClick={() => checkTask(box.id)}>
+                    Done
+                </button>
+            </div>)}
+        </div>
+
+        <div className='done-task-container'>
+            {doneBoxes.map(doneBox => 
+            <div className='my-done-task'>
+                <button className='delete-task-button' onClick={() => deleteTask(doneBox.id)}>
+                    Delete
+                </button>
+            </div>)}
+        </div>
     </div>
 }
 
